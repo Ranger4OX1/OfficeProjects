ce27b600a7805c5a05f212e6f3fa4c1ea66e5fb3
Ranger <ehtishamma21@gmail.com>
2022-07-23 00:07:39 +0500

    23/07/2022 00:07

---
+++ testApp1/testApp1/testApp1/DAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//NEW
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+
+namespace testApp1
+{
+    internal class DAL
+    {
+        public static SqlConnection GetConnection()
+        {
+            string sql = @"Data Source = .\SQLEXPRESS;
+                            Initial Catalog = test;
+                            Integrated Security = true ";
+            SqlConnection conn = new SqlConnection(sql);
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Sql Connected.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Connection error\n" + ex.Message,"Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //throw;
+            }
+            return conn;
+        }
+    }
+}
